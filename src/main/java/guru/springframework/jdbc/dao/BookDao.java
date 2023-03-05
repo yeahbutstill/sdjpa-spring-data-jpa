4c67513ba4b6344b8901ff9de2198892af2b308b
yeahbutstill <mayadani28@gmail.com>
2023-03-05 21:30:03 +0700

    Implement book dao

---
+++ src/main/java/guru/springframework/jdbc/dao/BookDao.java
@@ -0,0 +1,17 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Book;
+
+public interface BookDao {
+
+    Book getById(Long id);
+
+    Book findBookByTitle(String title);
+
+    Book saveNewBook(Book book);
+
+    Book updateBook(Book book);
+
+    void deleteBookById(Long id);
+
+}
---
+++ src/main/java/guru/springframework/jdbc/dao/BookDaoImpl.java
@@ -0,0 +1,52 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Book;
+import guru.springframework.jdbc.repositories.BookRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookDaoImpl implements BookDao {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookDaoImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book getById(Long id) {
+        return bookRepository.getReferenceById(id);
+    }
+
+    @Override
+    public Book findBookByTitle(String title) {
+        return bookRepository.findBookByTitle(title).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public Book saveNewBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Transactional
+    @Override
+    public Book updateBook(Book book) {
+        Book foundBook = bookRepository.getReferenceById(book.getId());
+        foundBook.setIsbn(book.getIsbn());
+        foundBook.setPublisher(book.getPublisher());
+        foundBook.setAuthorId(book.getAuthorId());
+        foundBook.setTitle(book.getTitle());
+
+        return bookRepository.save(foundBook);
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+}
