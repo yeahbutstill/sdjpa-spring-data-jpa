2e271293eeb7ea3fe1439539bd9584db4bc34858
yeahbutstill <mayadani28@gmail.com>
2023-03-09 10:43:04 +0700

    Pagging with hibernate

--- src/main/java/guru/springframework/jdbc/dao/BookDaoHibernate.java
+++ src/main/java/guru/springframework/jdbc/dao/BookDaoHibernate.java
@@ -28,7 +28,21 @@ public class BookDaoHibernate implements BookDao {
 
     @Override
     public List<Book> findAllBooks(Pageable pageable) {
-        return null;
+
+        EntityManager em = getEntityManager();
+
+        try {
+
+            TypedQuery<Book> query = em.createQuery("SELECT b FROM Book b", Book.class);
+            query.setFirstResult(Math.toIntExact(pageable.getOffset()));
+            query.setMaxResults(pageable.getPageSize());
+
+            return query.getResultList();
+
+        } finally {
+            em.close();
+        }
+
     }
 
     @Override
